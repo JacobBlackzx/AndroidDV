e7e841e749d783ab91e38cb8260690dcc8d326ea
Vitor Hugo <ti.vicktorzx@gmail.com>
2022-11-26 18:45:21 -0300

    Atualização Lógica e de Interface

--- SorteadorGoodLucky/app/src/main/java/com/ubuntu/android/sorteadorgoodlucky/MainActivity.java
+++ SorteadorGoodLucky/app/src/main/java/com/ubuntu/android/sorteadorgoodlucky/MainActivity.java
@@ -3,6 +3,10 @@ package com.ubuntu.android.sorteadorgoodlucky;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sortear(View view) {
+
+        TextView texto = findViewById(R.id.textResultado);
+
+        int x = new Random().nextInt(11);
+
+        texto.setText("Número Sorteador: "+x);
+
+
+
     }
 }
